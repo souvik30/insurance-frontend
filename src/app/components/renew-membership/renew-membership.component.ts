89cc314a23f3385b5c7d9850d606576794eb2b45
Souvik Mondal <souvikmondal1997@gmail.com>
2022-09-01 13:20:18 +0530

    Fixed Wrong Date Generation on some devices

--- src/app/components/renew-membership/renew-membership.component.ts
+++ src/app/components/renew-membership/renew-membership.component.ts
@@ -25,7 +25,7 @@ export class RenewMembershipComponent implements OnInit {
 
   ngOnInit(): void {
     this.issue_date=this.issue_date.concat(((new Date()).getFullYear()).toString(),"-",((new Date()).getMonth()+1).toString(),"-",((new Date()).getDate()).toString());
-    console.log(this.issue_date);
+    //console.log(this.issue_date);
     this.payment['ID_NUMBER']=this.id;
     this.payment['EXPIRY_DATE']=this.expiry_date;
     this.payment['ISSUE_DATE']=this.issue_date;
